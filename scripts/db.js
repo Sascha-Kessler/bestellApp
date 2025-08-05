12276ac510337471a4b71531ee052f25246dcef1
SaschaKessler90 <sascha.k1809@gmail.com>
2025-08-05 15:12:33 +0200

    add dishes db

---
+++ scripts/db.js
@@ -0,0 +1,17 @@
+let myDishes = [
+    {
+        "name": "Pizza Margerita",
+        "price": 9.00,
+        "description": "mit Tomatensauce und Käse",
+    },
+    {
+        "name": "Pizza Salami",
+        "price": 9.50,
+        "description": "mit Salami und Oregano",
+    },
+    {
+        "name": "Pizza Ruccola",
+        "price": 10.50,
+        "description": "mit Ruccola und frischem Parmesan",
+    }
+]
